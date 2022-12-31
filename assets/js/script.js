ac719778ee45363490f73023b3283e94d4f9b6ae
dr-ishigami-senku <nu522696@gmail.com>
2022-12-31 13:47:01 +0800

    ganto muna mga pre.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,29 @@ $(document).ready(function(){
       $('#modal-sign').modal('show');
     }
   });
+    
+    
+$('#single-image-upload-1').change(function(e){
+var fileName = e.target.files[0].name;
+$("#single-name-display-1").html(fileName); 
+        }); 
+$('#single-image-upload-2').change(function(e){
+var fileName = e.target.files[0].name;
+$("#single-name-display-2").html(fileName); 
+        });     
+    
+$('#multiple-image-upload-1').change(function(){
+    for(var i = 0 ; i < this.files.length ; i++){
+      var fileName = this.files[i].name;
+      $('#multiple-name-display-1').append('<div>' + fileName + '</div>');
+    }
+ });    
+$('#multiple-image-upload-2').change(function(){
+    for(var i = 0 ; i < this.files.length ; i++){
+      var fileName = this.files[i].name;
+      $('#multiple-name-display-2').append('<div>' + fileName + '</div>');
+    }
+ });     
+    
+    
 });
